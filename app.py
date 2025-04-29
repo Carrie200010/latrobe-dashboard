ebd90e71dd7aa09d4245d0c204329eff0e857847
Carrie200010 <dzy202407@gmail.com>
2025-04-29 22:26:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -154,6 +154,6 @@ def update_chat(chat_data):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8080)）
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
 
